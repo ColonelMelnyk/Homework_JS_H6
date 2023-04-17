0521962504ae6a89cf9f26ffc880887bb03770fa
ColonelMelnyk <xiaomiredmia15@gmail.com>
2023-04-17 10:47:23 +0300

    task 9 done, HW done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnColorEl = document.querySelector(`button[type ="button"]`);
+const textColorEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+btnColorEl.addEventListener("click", colorSwitch);
+function colorSwitch(){
+ getRandomHexColor();
+ textColorEl.textContent = `${getRandomHexColor()}`;
+ bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+};
